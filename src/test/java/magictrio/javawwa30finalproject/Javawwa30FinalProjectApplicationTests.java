b302ae07bef4d3f6549ad8aad837857a0c873d3c
Bartosz Arenin <bartosz.arenin@gmail.com>
2021-09-04 12:08:40 +0200

    Deleted JPA dependency

--- src/test/java/magictrio/javawwa30finalproject/Javawwa30FinalProjectApplicationTests.java
+++ src/test/java/magictrio/javawwa30finalproject/Javawwa30FinalProjectApplicationTests.java
@@ -3,11 +3,14 @@ package magictrio.javawwa30finalproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Javawwa30FinalProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
