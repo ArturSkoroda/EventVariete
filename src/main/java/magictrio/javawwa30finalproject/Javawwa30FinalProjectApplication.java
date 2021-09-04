9bf247cba20e40018c3c71deead3407ddad41e62
Artur <artur.skoroda5@gmail.com>
2021-09-04 11:18:35 +0200

    First commit

---
+++ src/main/java/magictrio/javawwa30finalproject/Javawwa30FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package magictrio.javawwa30finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javawwa30FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javawwa30FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/magictrio/javawwa30finalproject/Javawwa30FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package magictrio.javawwa30finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javawwa30FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
